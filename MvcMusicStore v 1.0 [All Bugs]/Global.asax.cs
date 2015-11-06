4301d25656c58e3893b1e7374c7bdd2871d1357d
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-11-07 00:45:09 +0600

    Version 1.0

---
+++ MvcMusicStore v 1.0 [All Bugs]/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            // telling the `Global.asax` file to initialize the SimpleData Model as Database
+
+            System.Data.Entity.Database.SetInitializer(new MvcMusicStore.Models.SampleData());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
